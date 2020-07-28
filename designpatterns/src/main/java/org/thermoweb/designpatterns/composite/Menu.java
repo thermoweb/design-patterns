09ab5e9e2a71ecff6fb639b9d7a248efea54da7f
thermo <romain.chauveau+github@protonmail.com>
2020-07-29 00:15:40 +0200

    composite

---
+++ designpatterns/src/main/java/org/thermoweb/designpatterns/composite/Menu.java
@@ -0,0 +1,54 @@
+package org.thermoweb.designpatterns.composite;
+
+import java.util.ArrayList;
+
+public class Menu extends MenuComponent {
+    private ArrayList<MenuComponent> menuComponents = new ArrayList<>();
+    private String name;
+    private String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return menuComponents.get(i);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void print() {
+        System.out.print("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("----------------------");
+
+        for (MenuComponent menuComponent : menuComponents) {
+            menuComponent.print();
+        }
+    }
+}
